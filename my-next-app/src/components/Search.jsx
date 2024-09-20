aecc3758bf021131b020df8e4b88173d1b96158a
Jonas <jonas.mokawane98@gmail.com>
2024-09-20 16:06:12 +0200

    Updated Search component: modified form layout, added SVG icon to submit button, and adjusted CSS classes.

--- my-next-app/src/components/Search.jsx
+++ my-next-app/src/components/Search.jsx
@@ -1,11 +1,38 @@
 export default function Search() {
     return (
-      <form action="/search" method="GET" className="">
-        <input name="query" placeholder="Search..." className="rounded-tl-2xl rounded-bl-2xl pl-3" />
-        <button type="submit" className="px-1 bg-white rounded-tr-2xl rounded-br-2xl ml-1 bg-blue-900">
-            <span className="px-1 text-xs font-bold items-baseline text-white">Submit</span>
-        </button>
-      </form>
+<form action="/search" method="GET" className="flex items-center">
+  <input
+    name="query"
+    placeholder="Search..."
+    className="rounded-tl-2xl rounded-bl-2xl pl-3"
+  />
+  <button
+    type="submit"
+    className="flex items-center justify-center px-1 bg-white rounded-tr-2xl rounded-br-2xl ml-1 bg-blue-900"
+  >
+    <span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        id="Capa_1"
+        x="0px"
+        y="0px"
+        viewBox="0 0 513.749 513.749"
+        style={{ enableBackground: "new 0 0 513.749 513.749" }}
+        width="512"
+        height="512"
+        className="h-4 w-7 m-1"
+      >
+        <g>
+          <path
+            d="M504.352,459.061l-99.435-99.477c74.402-99.427,54.115-240.344-45.312-314.746S119.261-9.277,44.859,90.15   S-9.256,330.494,90.171,404.896c79.868,59.766,189.565,59.766,269.434,0l99.477,99.477c12.501,12.501,32.769,12.501,45.269,0   c12.501-12.501,12.501-32.769,0-45.269L504.352,459.061z M225.717,385.696c-88.366,0-160-71.634-160-160s71.634-160,160-160   s160,71.634,160,160C385.623,314.022,314.044,385.602,225.717,385.696z"
+            fill="white"
+          />
+        </g>
+      </svg>
+    </span>
+  </button>
+</form>
     );
   }
   
