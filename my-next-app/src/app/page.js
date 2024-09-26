99024963c505a366565356a0ddfc3101faad366a
Jonas <jonas.mokawane98@gmail.com>
2024-09-26 08:53:49 +0200

    Refactored Products component

--- my-next-app/src/app/page.js
+++ my-next-app/src/app/page.js
@@ -1,14 +1,8 @@
-import Products from "./products/page";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <main>
-      <h1 className="text-center text-2xl font-bold mb-6">Welcome to Our Store</h1>
-         <Products />
-      </main>
-      <footer>
-      </footer>
-    </div>
-  );
+  const router = useRouter();
+  router.push("/products")
 }
---
+++ my-next-app/src/app/products/page.js
@@ -0,0 +1,14 @@
+import Products from "@/components/Products";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+      <h1 className="text-center text-2xl font-bold mb-6">Welcome to Our Store</h1>
+         <Products />
+      </main>
+      <footer>
+      </footer>
+    </div>
+  );
+}
